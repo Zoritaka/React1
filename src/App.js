0cb1be5eacb89016135adb067200689b7e9fc3c3
Luca <Dikhanbaev.2018@mail.ru>
2023-08-28 17:16:32 +0600

    step 3 to finish

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './pages/home';
 import MyBook from './pages/MyBook';
 import MyBand from './pages/MyBand';
+import MyRecipe from './pages/MyRecipe';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/" element={<Home/>}></Route>
         <Route path="/MyBook" element={<MyBook/>}></Route>
         <Route path="/MyBand" element={<MyBand/>}></Route>
+        <Route path="/MyRecipe" element={<MyRecipe/>}></Route>
       </Routes>
     </Router>
   );
