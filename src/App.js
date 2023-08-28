b06991634a233b4b10cf57350a7328a228f2e607
Luca <Dikhanbaev.2018@mail.ru>
2023-08-28 17:00:55 +0600

    step 2 to finish

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 import Home from './pages/home';
 import MyBook from './pages/MyBook';
+import MyBand from './pages/MyBand';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home/>}></Route>
         <Route path="/MyBook" element={<MyBook/>}></Route>
+        <Route path="/MyBand" element={<MyBand/>}></Route>
       </Routes>
     </Router>
   );
--- src/pages/MyBand.js
+++ src/pages/MyBand.js
@@ -5,13 +5,21 @@ const MyBand = () => {
         <div>
             <ul>
                 <li>Название группы: Slipknot</li>
-                <li>Участники: Михаила Булгакова</li>
-                <li>Альбомы: повесть</li>
+                <li>Участники: Shawn Crahan, Corey Taylor, Mick Thomson,<br/> Craig Jones, Jim Root, Sid Wilson, Chris Fehn, Joey Jordison</li>
+                <li>Жанры: Alternative Metal, Heavy Metal, Nü Metal, Rap-Metal</li>
+                <li>Альбомы:</li>
                 <div>
-                    <li>Slipknot</li>
-                    <img src="https://rovimusic.rovicorp.com/image.jpg?c=5jGWcSit2zbwxWaVhRVJst_M69_UI9rrJSVvWL2-yAg=&f=4" width={100} height={100}></img>
+                    <ol>
+                        <li>Slipknot: June 29, 1999</li>
+                        <img src="https://rovimusic.rovicorp.com/image.jpg?c=5jGWcSit2zbwxWaVhRVJst_M69_UI9rrJSVvWL2-yAg=&f=4" width={100} height={100}></img>
+
+                        <li>IOWA:  August 28, 2001</li>
+                        <img src="https://rovimusic.rovicorp.com/image.jpg?c=B0ZmNCXFiA_6iT_E1bJeqipQg_7iAU1wjqLgK_xGXts=&f=4" width={100} height={100}></img>
+
+                        <li>Vol. 3: The Subliminal Verses:  May 25, 2004</li>
+                        <img src="https://rovimusic.rovicorp.com/image.jpg?c=jHkiulVbTCi54VrskyfO4hyhM-OFI8zG4l-qVpXXB1I=&f=4" width={100} height={100}></img>
+                    </ol>
                 </div>
-                <li></li>
             </ul>
         </div>
     );
